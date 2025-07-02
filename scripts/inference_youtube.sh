b11afaa977ae9d086a758ae048564fb914728ee9
Neil Z. SHAO <initialneil@gmail.com>
2025-07-02 20:58:56 +0800

    - fix distribute on ubuntu

--- scripts/inference_youtube.sh
+++ scripts/inference_youtube.sh
@@ -15,15 +15,4 @@ python -m torch.distributed.launch \
     --output_dir demo/${OUTPUT_DIR}
 
 # Example usage:
-# python -m torch.distributed.launch --nproc_per_node 1 main.py",
-# "-c", "config/aios_smplx_youtube.py", 
-# "--options", 
-# "batch_size=6",
-# "--resume", "data/checkpoint/aios_checkpoint.pth", 
-# "--eval", 
-# "--inference", 
-# // "--to_vid", 
-# "--inference_input", 
-# "/mnt/f/Datasets/YoutubeGestureDataset/frame_segments_2fps", 
-# "--output_dir", 
-# "/mnt/f/Datasets/YoutubeGestureDataset/AiOS",
+# python -m torch.distributed.launch --nproc_per_node 4 main.py -c config/aios_smplx_youtube.py --options batch_size=8 --resume data/checkpoint/aios_checkpoint.pth --eval --inference --inference_input /mnt/f/Datasets/YoutubeGestureDataset/frame_segments_2fps --output_dir /mnt/f/Datasets/YoutubeGestureDataset/AiOS
